34684caa9edf49acdb71bc34f5c9a8ee45dca6bc
Asai Shinji <shinjia305@gmail.com>
2022-02-10 20:52:41 +0900

    Vuetifyをインストール

--- frontend/src/components/Calendar.vue
+++ frontend/src/components/Calendar.vue
@@ -1,31 +1,25 @@
 <template>
   <div>
-    <h1>Calendar</h1>
-    <ul>
-      <li v-for="event in events" :key="event.id">
+    <h1 class="text-h1">Calendar</h1>
+    <v-list>
+      <v-list-item v-for="event in events" :key="event.id">
         {{ event.name }}
-      </li>
-    </ul>
-    <button type="submit" @click="fetchEvents()">fetchEvents</button>
-
-    <CalendarDetails />
+      </v-list-item>
+    </v-list>
+    <v-btn type="submit" @click="fetchEvents()">fetchEvents</v-btn>
   </div>
 </template>
 
 <script>
 import { mapGetters, mapActions } from 'vuex';
-import CalendarDetails from './CalendarDetails.vue';
 
 export default {
   name: 'Calendar',
-  components: {
-    CalendarDetails,
-  },
   computed: {
     ...mapGetters('events', ['events']),
   },
   methods: {
-    ...mapActions('events', ['fetchEvents'])
-  }
+    ...mapActions('events', ['fetchEvents']),
+  },
 };
 </script>
